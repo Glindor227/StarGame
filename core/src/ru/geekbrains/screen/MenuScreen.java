5bc1f7e960da7ba41c0129c1dc269a4e8718350f
Glindor227 <glindor227@gmail.com>
2019-04-18 18:23:29 +0300

    ПЗ к Уроку 3

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -2,11 +2,66 @@ package ru.geekbrains.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
+
+    private Texture bg,lg;
+    private Background background;
+    private Logo logo;
+
+
+
+    @Override
+    public void show() {
+        super.show();
+        bg = new Texture("bg.png");
+        lg = new Texture("badlogic.jpg");
+        background = new Background(new TextureRegion(bg));
+        logo = new Logo(new TextureRegion(lg));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        logo.resize(worldBounds);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        batch.begin();
+        background.draw(batch);
+        logo.update(delta);
+        logo.draw(batch);
+        batch.end();
+    }
+
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        lg.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        System.out.println(pointer);
+        logo.touchDown(touch,pointer);
+        return false;
+    }
+
+}
+
+class MenuScreen_OLD extends BaseScreen {
 
     private Vector2 touch,tauchTemp;
     private Vector2 pos;
